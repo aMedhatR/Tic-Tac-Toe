0e3221d1b599a5106111a36f4e7261420ed42b7c
nora-alaa <96909010+nora-alaa@users.noreply.github.com>
2022-01-25 21:48:14 +0200

    Delete Client directory

--- Client/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.mavenprojettest {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.mavenprojettest to javafx.fxml;
-    exports com.mycompany.mavenprojettest;
-}
